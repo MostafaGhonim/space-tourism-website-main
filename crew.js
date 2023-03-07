611a42281b86bbfcf83f13850249b4c3531ba140
MostafaGhonim <91387222+MostafaGhonim2@users.noreply.github.com>
2023-03-07 23:11:56 +0200

    fix images size

--- crew.js
+++ crew.js
@@ -27,7 +27,7 @@ fetch('./data.json').then((e) => {
                     if (e.target.id === `dot1`) {
                         photoCrew.style.width = '350px'
                     } else {
-                        photoCrew.style.width = '360px'
+                        photoCrew.style.width = '350px'
                     }
                     titlePosation.innerHTML = crewInformation[i]["role"];
                     namePerson.innerHTML = crewInformation[i]["name"];
